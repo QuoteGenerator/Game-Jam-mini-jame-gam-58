1688f1362e81b108f9d759da170566a197efb0fc
sekitano <mauziofhope07@gmail.com>
2026-08-23 09:29:15 +0100

    menu screen chime added on pressing play, another transition added

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,6 +4,10 @@ extends Node2D
 @onready var fade: AnimationPlayer = $fade_screen/AnimationPlayer
 @onready var fade_screen: ColorRect = $fade_screen
 @onready var start: Button = $Button_Manager/Start
+@onready var church_bell: AudioStreamPlayer = $ChurchBell
+@onready var blackscreen: ColorRect = $blackscreen
+@onready var label: Label = $Label
+@onready var switch_timer: Timer = $SwitchTimer
 
 #var button_type = null
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +16,17 @@ func _ready() -> void:
 
 func _on_start_pressed() -> void:
 	#button_type = "start"
+	church_bell.play()
 	fade_screen.show()
 	start.hide()
 	fade.play("fade_in")
 	timer.start()
 	
 func _on_timer_timeout() -> void:
+	label.show()
+	blackscreen.show()
+	switch_timer.start()
+
+
+func _on_switch_timer_timeout() -> void:
 	get_tree().call_deferred("change_scene_to_file", "res://Scenes/StarterArea.tscn")
